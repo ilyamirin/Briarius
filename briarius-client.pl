4cddd01dba585c9dce5d4db74b648268ac171440
Ilya G. Mirin <ilya.mirin@gmail.com>
2013-04-17 16:19:27 +1100

    Briarius can backup files now.

--- briarius-client.pl
+++ briarius-client.pl
@@ -5,6 +5,7 @@ use Data::Dumper;
 use Log::Log4perl qw(:easy);
 use Mojo::UserAgent;
 use Mojo::JSON 'j';
+use Digest::MD5 qw(md5_hex);
 
 use constant {
     #working parameters
@@ -12,53 +13,15 @@ use constant {
 };
 
 my $args;
-
-sub process_file {
-    my ( $path_to_file, $tx ) = @_;
-
-    if ( -d $path_to_file ) {
-        INFO 'Directory has been found: ' . $path_to_file;
-
-        my $dir;
-        opendir $dir, $path_to_file or die $!;
-
-        while ( readdir $dir ) {
-            next if $_ =~ /^\.\.?$/;
-            $path_to_file .= '/' unless $path_to_file =~ /\/$/;
-            process_file( $path_to_file . $_ , $tx );
-        }
-
-        closedir $dir;
-
-    } elsif ( -f $path_to_file ) {
-        INFO 'File has been found: ' . $path_to_file;        
-
-        #TODO:: can i backup file?
-        #$tx->send( j { request => 'IS_FILE_VERSION_EXIST', client => $args->{ '-n' } } );
-
-        my $file;
-        open $file, "<", $path_to_file or die $!;
-
-        flock $file, 1;
-
-        my ( $buf, $data, $n );
-        while ( ( $n = read $file, $data, CHUNK_SIZE ) > 0 ) {
-            INFO "$n bytes hav been readed.";
-            $buf .= $data;
-        }
-
-        close $file;
-
-    }#elsif
-
-}#sub process_file
-
 my @files = ();
+my $target_to_local = {};
+my $chunks = {};
+my $chunks_to_send = {};
 
-sub get_target_path {
+sub get_path_in_target {
     my ( $absolute_path, $outer_path ) = @_;
     if ( $outer_path =~ /^$absolute_path\/(.+)$/ ) {
-        return $1;
+        return $args->{ '--target-id' } . '/' . $1;
     }
     else {
         ERROR "Incorrect path $absolute_path, $outer_path";
@@ -83,20 +46,64 @@ sub crawl {
         closedir $dir;
 
     } elsif ( -f $path_to_file ) {
-        INFO 'File has been found: ' . $path_to_file;    
-        push @files, $path_to_file;    
+        INFO 'File has been found: ' . $path_to_file;
+        my $path_to_file_in_target = get_path_in_target( $args->{ '-p' }, $path_to_file );
+        push @files, $path_to_file_in_target;        
+        $target_to_local->{ $path_to_file_in_target } = $path_to_file;
 
     }#elsif
 
 }#sub process_file
 
+sub slice_file {
+    my $path_to_file_in_target = shift;
+    my $path_to_file = $target_to_local->{ $path_to_file_in_target };
+
+    my $file;
+    open $file, '<', $path_to_file or die $!;
+
+    $chunks->{ $path_to_file_in_target } = [] and return if (stat( $file ))[ 7 ] == 0;
+
+    INFO $path_to_file;
+    flock $file, 1;
+
+    my ( $data, $n );
+    while ( ( $n = read $file, $data, CHUNK_SIZE ) > 0 ) {
+        #INFO "$n bytes have been readed from $file";
+        push @{ $chunks->{ $path_to_file_in_target } }, $data;
+    }
+
+    close $file;
+}
+
+sub ws_is_next_file_exist {
+    my $tx = shift;
+    if ( my $file = shift @files ) {
+        my $req = { request => 'IS_FILE_VERSION_EXISTED', client => $args->{ '-n' }, file => $file };
+        $tx->send( j $req );
+    }
+}
+
+sub ws_is_next_chunk_existed {
+    my ( $tx, $msg ) = @_;
+    my $chunk = shift @{ $chunks->{ $msg->{ file } } };
+    return 0 unless $chunk;
+    my $hash = md5_hex $chunk;
+    my $req = { request => 'IS_CHUNK_EXISTED', chunk_hash => $hash };
+    $req->{ client } = $args->{ '-n' };
+    $req->{ $_ } = $msg->{ $_ } for qw/file file_version/;
+    $tx->send( j $req );
+    $chunks_to_send->{ $hash } = $chunk;
+}
+
+sub ws_create_file_version {
+    my ( $tx, $msg ) = @_;
+    my $req = { request => 'CREATE_FILE_VERSION', file_version => $msg->{ file_version } };
+    $tx->send( j $req );
+}
+
 BEGIN {
-	Log::Log4perl->easy_init( 
-#		{ 
-#			level    => 'DEBUG',
-#			file     => ">>briarius-client.log",
-#			layout   => '%F{1}-%L-%M: %m%n' 
-#		},
+	Log::Log4perl->easy_init(
 		{ 
 			level    => 'DEBUG',
             file     => "STDOUT",
@@ -130,20 +137,54 @@ BEGIN {
             $msg = j $msg;
             if ( $msg->{ response } eq 'BACKUP_START_ACCEPTED' ) {
                 INFO 'Backup start accepted by server';
-                my $file = $args->{ '--target-id' } . '/' . get_target_path( $args->{ '-p' }, shift( @files ) );
-                $tx->send( j { request => 'IS_FILE_VERSION_EXISTED', client => $args->{ '-n' }, file => $file } );
-
+                ws_is_next_file_exist( $tx );
             } 
             elsif ( $msg->{ response } eq 'FILE_VERSION_IS_NOT_EXISTED' ) {
                 INFO "File version $msg->{ file_version } is not existed";
-                #start backup file
+                slice_file $msg->{ file };
+                ws_is_next_file_exist( $tx ) unless ws_is_next_chunk_existed( $tx, $msg );
             }
             elsif ( $msg->{ response } eq 'FILE_VERSION_IS_EXISTED' ) {
                 INFO "File version $msg->{ file_version } is existed";
-                
+                ws_is_next_file_exist( $tx );
+            }
+            elsif ( $msg->{ response } eq 'CHUNK_IS_NOT_EXISTED' ) {
+                INFO "Chunk $msg->{ chunk_hash } is not existed.";
+                if ( $chunks_to_send->{ $msg->{ chunk_hash } } ) {
+                    my $req = { 
+                        request      => 'LOAD_CHUNK', 
+                        client       => $args->{ '-n' },
+                        file         => $msg->{ file }, 
+                        file_version => $msg->{ file_version }, 
+                        chunk_hash   => $msg->{ chunk_hash }, 
+                        chunk        => $chunks_to_send->{ $msg->{ chunk_hash } }
+                    };
+                    $tx->send( j $req );
+                    $chunks_to_send->{ $msg->{ chunk_hash } } = undef;
+                }
+            }
+            elsif ( $msg->{ response } eq 'CHUNK_IS_EXISTED' ) {
+                INFO "Chunk $msg->{ chunk_hash } is existed.";
+                my $req = { 
+                    request      => 'ADD_EXISTED_CHUNK_TO_FILE_VERSION', 
+                    client       => $args->{ '-n' },
+                    file         => $msg->{ file }, 
+                    file_version => $msg->{ file_version }, 
+                    chunk_hash   => $msg->{ chunk_hash }
+                };
+                $tx->send( j $req );
+                $chunks_to_send->{ $msg->{ chunk_hash } } = undef;
+            }
+            elsif ( $msg->{ response } eq 'CHUNK_WAS_LOADED' ) {
+                INFO "Chunk $msg->{ chunk_hash } was loaded.";
+                ws_create_file_version( $tx, $msg ) unless ws_is_next_chunk_existed( $tx, $msg );
+            }
+            elsif ( $msg->{ response } eq 'FILE_VERSION_WAS_CREATED' ) {
+                INFO "File version $msg->{ file_version } was created.";
+                ws_is_next_file_exist( $tx );
             }
             else {
-                ERROR "Unknown server response $msg";
+                ERROR 'Unknown server response ' . Dumper $msg;
                 $tx->finish;
             }		
 		});
--- briarius-server.pl
+++ briarius-server.pl
@@ -2,7 +2,10 @@
 use Mojolicious::Lite;
 
 use DateTime;
+use Data::Dumper;
+use File::Path qw(make_path remove_tree);
 use Mojo::JSON 'j';
+#use Time::HiRes qw/ time sleep /;
 
 plugin 'PODRenderer';
 
@@ -23,49 +26,40 @@ sub is_file_version_existed {
 	return -e FILES_REGISTRY . shift;
 }
 
-sub create_empty_file_version {
-	my ( $self, $user_name, $target_name, $file_name, $time ) = @_;
-	
-	my $key = create_file_version_key( $user_name, $target_name, $file_name, $time );
-	
-	unless ( -e FILES_REGISTRY . $key . INCOMPLETE_ENDING ) {
-		$self->app->log->error( "Incomplete file version for $key has not been created yet!" );
-		return 0;
-	}
-
-	$self->app->log->info( "Create file version: $key" );
-	rename FILES_REGISTRY . $key . INCOMPLETE_ENDING => FILES_REGISTRY . $key;	
-	return 1;
-}
-
 sub add_chunk_to_version {
-	my ( $self, $user_name, $target_name, $file_name, $time, $chunk_hash ) = @_;
-	my $key = create_file_version_key( $user_name, $target_name, $file_name, $time );
-	$self->app->log->info( "Start chunk to file version addition. Chunk hash: $chunk_hash, file version: $key" );
-    my $file;
-    open( $file, '>>', FILES_REGISTRY . $key . INCOMPLETE_ENDING ) or $self->app->log->error( $! ) and return 0;
-    print "$chunk_hash\n", $file;
+	my ( $chunk_hash, $file_version ) = @_;
+	$file_version =~ /^(.+)\/[^\/]+$/;
+	make_path FILES_REGISTRY . $1;
+	say FILES_REGISTRY . $file_version . INCOMPLETE_ENDING;
+    open( my $file, '>>', FILES_REGISTRY . $file_version . INCOMPLETE_ENDING ) or return 0;
+    print $file "$chunk_hash\n";
     close $file;
     return 1;
 }
 
 sub is_chunk_existed {
-	my ( $self, $chunk_hash ) = @_;
-	return -e CHUNCKS_REGISTRY . $chunk_hash;
+	return -e CHUNCKS_REGISTRY . shift;
 }
 
 sub create_chunk {
-	my ( $self, $chunk_hash, $chunk_content ) = @_;
-	if ( is_chunk_existed $chunk_hash ) {
-		$self->app->log->error( "Chunck with hash $chunk_hash has already been created!" );
+	my ( $chunk_hash, $chunk_content ) = @_;
+	if ( -e CHUNCKS_REGISTRY . $chunk_hash ) {
 		return 0;
 	}
-	$self->app->log->info( "Chunk with hash $chunk_hash is backuping now" );
-    my $file;
-    open( $file, '>>', CHUNCKS_REGISTRY . $chunk_hash ) or $self->app->log->error( $! ) and return 0;
-    print $chunk_content, $file;
-    close $file;	
+    $chunk_hash =~ s/\//--/g;
+    open( my $file, '>', CHUNCKS_REGISTRY . $chunk_hash ) or return 0;
+    print $file $chunk_content;
+    close $file;
 	return 1;
+}
+
+sub create_file_version {
+	my ( $file_version ) = @_;
+	my $path_to_version = FILES_REGISTRY . $file_version;
+	my $path_to_incomplete_version = $path_to_version . INCOMPLETE_ENDING;
+	return ( 0, "File version $file_version is already existed" ) if -e $path_to_version;
+	return ( 0, "Has no incomplete $file_version" ) unless -e $path_to_incomplete_version;
+	return rename $path_to_incomplete_version, $path_to_version;
 }
 
 sub on_message {
@@ -82,13 +76,69 @@ sub on_message {
 		$self->app->log->info( "Client $msg->{ client } ask: is file version for file $msg->{ file } existed?" );
 
 		my $user = 'Ivan';
-		my $key = create_file_version_key( $user, $msg->{ file }, time() );
+		my $key = create_file_version_key( $user, $msg->{ file }, time );
 		if ( is_file_version_existed $key ) {
 			$self->send( j { response => 'FILE_VERSION_IS_EXISTED', file => $msg->{ file }, file_version => $key } );
 		}
 		else {
 			$self->send( j { response => 'FILE_VERSION_IS_NOT_EXISTED', file => $msg->{ file }, file_version => $key } );
 		}
+	}
+	elsif ( $msg->{ request } eq 'IS_CHUNK_EXISTED' ) {
+		$self->app->log->info( "Client $msg->{ client } ask: is chunk $msg->{ chunk_hash } for $msg->{ file_version } existed?" );
+		my $user = 'Ivan';
+		my $res = {};
+		$res->{ $_ } = $msg->{ $_ } for qw/chunk_hash file file_version/;
+		if ( is_chunk_existed $msg->{ chunk_hash } ) {			
+			$res->{ response } = 'CHUNK_IS_EXISTED';
+		}
+		else {
+			$res->{ response } = 'CHUNK_IS_NOT_EXISTED';
+		}
+		$self->send( j $res );
+	}
+	elsif ( $msg->{ request } eq 'LOAD_CHUNK' ) {		
+		my $user = 'Ivan';
+		my $response = {};
+		$response->{ $_ } = $msg->{ $_ } for qw/chunk_hash file file_version/;
+		unless ( create_chunk $msg->{ chunk_hash }, $msg->{ chunk } ) {
+			$self->app->log->error( "Cant load chunk $msg->{ chunk_hash }" ); 
+			$response->{ response } = 'CHUNK_WAS_NOT_LOADED';
+		}
+		else {
+			$response->{ response } = 'CHUNK_WAS_LOADED';			
+			unless ( add_chunk_to_version $msg->{ chunk_hash }, $msg->{ file_version } ) {
+				$self->app->log->error( "Cant add chunk to file" ); 
+				$response->{ response } = 'CHUNK_WAS_NOT_LOADED';
+			}
+		}
+		$self->send( j $response );
+	}
+	elsif ( $msg->{ request } eq 'ADD_EXISTED_CHUNK_TO_FILE_VERSION' ) {		
+		my $user = 'Ivan';
+		my $response = {};
+		$response->{ $_ } = $msg->{ $_ } for qw/chunk_hash file file_version/;		
+		unless ( add_chunk_to_version $msg->{ chunk_hash }, $msg->{ file_version } ) {
+			$self->app->log->error( "Cant add chunk to file" ); 
+			$response->{ response } = 'CHUNK_WAS_NOT_LOADED';
+		} 
+		else {
+			$response->{ response } = 'CHUNK_WAS_LOADED';
+		}
+		$self->send( j $response );
+	}
+	elsif ( $msg->{ request } eq 'CREATE_FILE_VERSION' ) {
+		my $response = {};
+		$response->{ $_ } = $msg->{ $_ } for qw/chunk_hash file file_version/;
+		my ( $status, $message ) = create_file_version $msg->{ file_version };		
+		if ( $status ) {
+			$response->{ response } = 'FILE_VERSION_WAS_CREATED';
+		}
+		else {
+			$self->app->log->error( $message );
+			$response->{ response } = 'FILE_VERSION_WAS_NOT_CREATED';
+		}
+		$self->send( j $response );
 	}
 }
 
